383102b654de224fa7263be63f0ebdb352116b2a
marcojakob <majakob@gmx.ch>
2015-02-25 21:22:57 +0100

    Scenarios part 5

---
+++ example/part5/scenario5.01-backing-a-cake/my_player.dart
@@ -0,0 +1,26 @@
+import 'package:hello_dart/hello_dart.dart';
+
+/// Your player.
+class MyPlayer extends Player {
+
+  /// Your program.
+  start() {
+  }
+
+  /// Puts [count] stars in a row.
+  putStars(int count) {
+  }
+
+  /// Makes a number of [steps] in the current direction.
+  multiMove(int steps) {
+  }
+
+  /// Turns around by 180 degrees.
+  turnAround() {
+  }
+}
+
+
+main() {
+  createWorld('scenario.txt', new MyPlayer());
+}
